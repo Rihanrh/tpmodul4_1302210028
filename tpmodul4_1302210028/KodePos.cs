b36ca2196c5365b27a51cf843c67aa22263496f3
Rihanrh <rihanh@student.telkomuniversity.ac.id>
2023-03-12 17:34:21 +0700

    Inisialisasi projek

---
+++ tpmodul4_1302210028/KodePos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1302210028
+{
+    internal class KodePos
+    {
+        public enum Kelurahan
+        {
+            Batununggal, Kujangsari, Mengger, Wates, Cijaura, Jatisari, Margasari, Sekejati, Kebonwaru, Maleer, Samoja
+        }
+
+        public int getKodePos(Kelurahan kelurahan)
+        {
+            int[] KodePos =
+            {
+            40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273
+        };
+            return KodePos[(int)kelurahan];
+        }
+    }
+}
---
+++ tpmodul4_1302210028/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using tpmodul4_1302210028;
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        KodePos cari = new KodePos();
+        KodePos.Kelurahan klu = KodePos.Kelurahan.Batununggal;
+        int kp = cari.getKodePos(klu);
+        Console.WriteLine("Kode Pos untuk kelurahan " + klu + " adalah " + kp);
+    }
+}
