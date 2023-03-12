83c37def5fbf96c2c6547e3da675335a6b6b2919
Rihanrh <rihanh@student.telkomuniversity.ac.id>
2023-03-12 18:42:13 +0700

    Menambahkan implementasi dengan state-based construction

---
+++ tpmodul4_1302210028/DoorMachine.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1302210028
+{
+    internal class DoorMachine
+    {
+        public enum DoorState { Terbuka, Terkunci};
+        public enum Trigger { BukaPintu, KunciPintu};
+
+        class transition
+        {
+            public DoorState prevState;
+            public DoorState nextState;
+            public Trigger trigger; 
+
+            public transition(DoorState prevState, DoorState nextState, Trigger trigger)
+            {
+                this.prevState = prevState;
+                this.nextState = nextState;
+                this.trigger = trigger;
+            }
+        }
+
+        transition[] transitions =
+        {
+            new transition(DoorState.Terbuka, DoorState.Terbuka, Trigger.BukaPintu),
+            new transition(DoorState.Terbuka, DoorState.Terkunci, Trigger.KunciPintu),
+            new transition(DoorState.Terkunci, DoorState.Terkunci, Trigger.KunciPintu),
+            new transition(DoorState.Terkunci, DoorState.Terbuka, Trigger.BukaPintu)
+        };
+
+        public DoorState currentState;
+
+        public DoorState getNextState(DoorState prevState, Trigger trigger)
+        {
+            DoorState nextState = prevState;
+
+            for (int i = 0; i < transitions.Length; i++)
+            {
+                if (transitions[i].prevState == prevState && transitions[i].trigger == trigger)
+                {
+                    nextState = transitions[i].nextState;
+                }
+            }
+            return nextState;
+        }
+
+        public void activeTrigger(Trigger trigger)
+        {
+            DoorState nextState = getNextState(currentState, trigger);
+            currentState = nextState;
+
+        }
+    }
+}
--- tpmodul4_1302210028/Program.cs
+++ tpmodul4_1302210028/Program.cs
@@ -6,8 +6,34 @@ internal class Program
     public static void Main(string[] args)
     {
         KodePos cari = new KodePos();
-        KodePos.Kelurahan klu = KodePos.Kelurahan.Batununggal;
-        int kp = cari.getKodePos(klu);
-        Console.WriteLine("Kode Pos untuk kelurahan " + klu + " adalah " + kp);
+        KodePos.Kelurahan batun = KodePos.Kelurahan.Batununggal;
+        int kp = cari.getKodePos(batun);
+        Console.WriteLine("Kode Pos untuk kelurahan " + batun + " adalah " + kp);
+        Console.WriteLine("\n");
+
+        DoorMachine doorMachine = new DoorMachine();
+        doorMachine.currentState = DoorMachine.DoorState.Terbuka;
+
+        Console.Write("Sekarang pintu: ");
+        if (doorMachine.currentState == DoorMachine.DoorState.Terbuka)
+        {
+            Console.WriteLine("Terbuka");
+        }
+        else
+        {
+            Console.WriteLine("Terkunci");
+        }
+        Console.WriteLine("Pintunya akan: (input y untuk membuka, n untuk mengunci)");
+        string input = Console.ReadLine();
+        if (input == "y")
+        {
+            doorMachine.activeTrigger(DoorMachine.Trigger.BukaPintu);
+            Console.Write("\nSekarang pintu: "+doorMachine.currentState);
+        }
+        else if (input == "n")
+        {
+            doorMachine.activeTrigger(DoorMachine.Trigger.KunciPintu);
+            Console.Write("\nSekarang pintu: " +doorMachine.currentState);
+        }
     }
 }
